a1bf9d748fc3294e0c4686a0cf72cbcd1c6539bc
Jeevan-practice <jeevankgowda2003@gmail.com>
2025-09-04 17:22:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("display.html", name=name)
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+print("Hello, Jeevan")
